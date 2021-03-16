708035939daf8f6a3c6a482623b19eba108c706b
jagrit khetarpal <jagrit.khetarpal@sourcefuse.com>
2021-03-16 12:55:01 +0530

    updating host

--- src/services/EnvService.js
+++ src/services/EnvService.js
@@ -4,6 +4,6 @@
 import axios from 'axios';
 
 export default class EnvService{
-  static host = 'https://31dae828f094.ngrok.io/';
+  static host = 'https://f8decbc07908.ngrok.io/';
   static apiClient = axios;
 }
